b1f9ac7099bc19512a9b41696cd3e654dd62cefd
Michael Cotterell <mepcotterell@gmail.com>
2012-03-08 14:13:14 -0500

    Added initial file

---
+++ src/fbpa/DataMiner.scala
@@ -0,0 +1,21 @@
+/* DataMiner.scala
+ * 
+ * @author  Michael E. Cotterell <mepcotterell@gmail.com>
+ * 
+ * @compile scalac -cp ../../classes:../../lib/restfb.jar -d ../../classes DataMiner.scala
+ * @run     scala -cp ../../classes:../../lib/restfb.jar fbpa.DataMiner 
+ *
+ */
+
+package fbpa
+
+import com.restfb._
+
+/** DataMiner
+ */
+object DataMiner extends App {
+
+  
+
+} // DataMiner
+
