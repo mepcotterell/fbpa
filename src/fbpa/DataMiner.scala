ed9bb13970a9324263aff72b651f5128e6faff13
Michael Cotterell <mepcotterell@gmail.com>
2012-03-19 16:34:55 -0400

    Initial version of the DataMiner

--- src/fbpa/DataMiner.scala
+++ src/fbpa/DataMiner.scala
@@ -3,19 +3,44 @@
  * @author  Michael E. Cotterell <mepcotterell@gmail.com>
  * 
  * @compile scalac -cp ../../classes:../../lib/restfb.jar -d ../../classes DataMiner.scala
- * @run     scala -cp ../../classes:../../lib/restfb.jar fbpa.DataMiner 
+ * @run     scala -cp ../../classes:../../lib/restfb.jar fbpa.DataMinerApp
  *
  */
 
 package fbpa
 
 import com.restfb._
+import com.restfb.types._
+
+case class DataMiner () {
+
+  /** This is a client that can only access publicly-visible data - no access token needed
+   */
+  val client: FacebookClient = new DefaultFacebookClient()
+
+  def publicSearchPost (q: String): Connection[Post] = {
+    client.fetchConnection("search", 
+			   classOf[Post], 
+			   Parameter.`with`("q", q), 
+			   Parameter.`with`("type", "post")
+    )
+  } // publicSearch
+
+} // DataMiner
 
 /** DataMiner
  */
-object DataMiner extends App {
+object DataMinerApp extends App {
 
-  
+  /** Instance of the DataMiner
+   */
+  val miner = DataMiner()
+
+
+  val result = miner.publicSearchPost("dubstep").getData
+
+  printf("Size of result: %d\n", result.size)
+  printf("Public search: %s\n", result.get(0).getMessage)
 
 } // DataMiner
 
